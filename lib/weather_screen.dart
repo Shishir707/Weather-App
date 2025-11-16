6cd08f8e8ff0e1439140c5fef1fb10b863839ae9
Shishir707 <mirshr8@gmail.com>
2025-11-16 22:07:10 +0600

    implement geo Api get latitude & longitude

--- lib/weather_screen.dart
+++ lib/weather_screen.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
 
 class WeatherScreen extends StatefulWidget {
   const WeatherScreen({super.key});
@@ -8,8 +11,52 @@ class WeatherScreen extends StatefulWidget {
 }
 
 class _WeatherScreenState extends State<WeatherScreen> {
+  ///---------- Network Call ---------------
+
+  Future<void> geoLocation(String city) async {
+    try {
+      final url = Uri.parse(
+        "https://geocoding-api.open-meteo.com/v1/search?name=$city&count=1&format=json",
+      );
+
+      final response = await get(url);
+      print('Response: ${response.body}');
+      if (response.statusCode != 200) {
+        throw Exception("Geo Coding Fail. Status Code: ${response.statusCode}");
+      }
+      final deData = jsonDecode(response.body) as Map<String, dynamic>;
+      final result = (deData['results'] as List?) ?? [];
+      if (result.isEmpty) throw Exception("City Not Found");
+
+      final m = result.first as Map<String, dynamic>;
+      final lat = (m['latitude'] as num).toDouble();
+      final lon = (m['longitude'] as num).toDouble();
+      final name = ("${m['name']}, ${m['country']}");
+
+      print('Lat: $lat , Lon: $lon , Name: $name');
+
+    } catch (e) {
+      throw Exception(e.toString());
+    }
+  }
+
+  @override
+  void initState() {
+    geoLocation("Dhaka");
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Weather App',
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+        backgroundColor: Colors.blue.shade200,
+        centerTitle: true,
+      ),
+    );
   }
 }
